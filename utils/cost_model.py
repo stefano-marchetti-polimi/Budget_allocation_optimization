b05861156bcedc376b7b3c8b04434989b56a9c61
thomascoscia <thomas.coscia98@gmail.com>
2025-11-06 18:05:18 +0100

    Add files via upload

--- utils/cost_model.py
+++ utils/cost_model.py
@@ -22,20 +22,33 @@ from typing import Dict, Iterable
 # Cost of flood defense systems (M€ per (km*m))
 # Aerts, J. C., Botzen, W. W., de Moel, H., & Bowman, M. (2013). Cost estimates for flood resilience and protection strategies in New York City. Annals of the New York Academy of Sciences, 1294(1), 1-104.
 
-#  Label   Typeoffloodwall  cost in (M€ per (km*m))
-#  "W76"   7-FootHighL-Wallwith6-FootWideMonoliths 3.7
-#  "W88"   8-FootHighT-Wallwith8-FootWideMonoliths 3.43
-#  "W108"  10-FootHighT-Wallwith8-FootWideMonoliths 3.31
-#  "W1211" 12-FootHighT-Wallwith11-FootWideMonoliths 3.43
-#  "W1411" 14-FootHighL-Wallwith11-FootWideMonoliths 3.63
-#  "W1611" 16-FootHighL-Wallwith11-FootWideMonoliths 3.53
-#  "W1813" 18-FootHighL-Wallwith13-FootWideMonoliths 3.72
-#  "W2014" 20-FootHighT-Wallwith14-FootWideMonoliths 3.99
-#  "W2216" 22-FootHighT-Wallwith16-FootWideMonoliths 3.96
-#  "W2417" 24-FootHighT-Wallwith17-FootWideMonoliths 4.1
-#  "W266"  26-FootHighL-Wallwith6-FootWideMonoliths 4.54
-#  "W286"  28-FootHighL-Wallwith6-FootWideMonoliths 4.47
-#  "W306"  30-FootHighL-Wallwith6-FootWideMonoliths 4.52
+# from Table 3.1 (2008)
+#  Label, Type of floodwall, cost in (M€ per (km*m))
+#  "W76", 7-FootHighL-Wallwith6-FootWideMonoliths, 3.7
+#  "W88", 8-FootHighT-Wallwith8-FootWideMonoliths, 3.43
+#  "W108", 10-FootHighT-Wallwith8-FootWideMonoliths, 3.31
+#  "W1211", 12-FootHighT-Wallwith11-FootWideMonoliths, 3.43
+#  "W1411", 14-FootHighL-Wallwith11-FootWideMonoliths, 3.63
+#  "W1611", 16-FootHighL-Wallwith11-FootWideMonoliths, 3.53
+#  "W1813", 18-FootHighL-Wallwith13-FootWideMonoliths, 3.72
+#  "W2014", 20-FootHighT-Wallwith14-FootWideMonoliths, 3.99
+#  "W2216", 22-FootHighT-Wallwith16-FootWideMonoliths, 3.96
+#  "W2417", 24-FootHighT-Wallwith17-FootWideMonoliths, 4.1
+#  "W266", 26-FootHighL-Wallwith6-FootWideMonoliths, 4.54
+#  "W286", 28-FootHighL-Wallwith6-FootWideMonoliths, 4.47
+#  "W306", 30-FootHighL-Wallwith6-FootWideMonoliths, 4.52
+
+# from Table 3.2
+#  Label, Type of floodwall, cost in (M€ per (km*m)) converted accordint to €/$=1.25 (2012)
+#  "WT24", Flood T-wall (24ft), 5.625
+#  "WL30", Flood L-wall (30ft), 5.875
+#  "D", Dike, 10
+#  "HD", Hurricane Dike, 11.25
+#  "RBH", Retrofit bulkhead, 10
+#  "ML", Mixed levee, 10
+#  "FPLD", Flood protection low density, 5
+#  "HL", Hidden levee + nourishment, 10
+
 
 DEFAULT_ASSET_COSTS: Dict[str, float] = {
     "W76": 3.7,
@@ -51,6 +64,14 @@ DEFAULT_ASSET_COSTS: Dict[str, float] = {
     "W266": 4.54,
     "W286": 4.47,
     "W306": 4.52,
+    "WT24": 5.625,
+    "WL30": 5.875,
+    "D": 10,
+    "HD": 11.25,
+    "RBH": 10,
+    "ML": 10,
+    "FPLD": 5,
+    "HL": 10,
 }
 
 def list_asset_types(asset_costs: Dict[str, float] | None = None) -> Iterable[str]:
